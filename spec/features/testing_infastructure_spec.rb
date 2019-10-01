e46eb8ca3aca2a79fa4e0c0e30787a0b350346da
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-10-01 17:38:11 +0100

    Forked code I was working on with John

---
+++ spec/features/testing_infastructure_spec.rb
@@ -0,0 +1,8 @@
+# require "./app.rb"
+# 
+# feature 'Testing infastructure' do
+#   scenario 'can check page contents' do
+#     visit('/')
+#     expect(page).to have_content 'Battle'
+#   end
+# end

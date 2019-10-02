b8b0886dde9919b72c7baa43f124836497399d81
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-10-02 16:32:50 +0100

    Skinny controller - access Player instance through Game

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,18 @@ require_relative 'player'
 
 class Game
 
+  def initialize(player1, player2)
+    @players = [player1, player2]
+  end
+
+  def player1
+    @players[0]
+  end
+
+  def player2
+    @players[1]
+  end
+
   def attack(player)
     player.receive_damage
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,28 @@
 require 'game'
 
 describe Game do
+  let(:player_1) { double :player_1, receive_damage: 10}
   let(:player_2) { double :player_2, receive_damage: 10}
+  let(:game) { Game.new(player_1, player_2) }
+
   describe '#attack' do
     it 'damages the player' do
       expect(player_2).to receive(:receive_damage)
-      subject.attack(player_2)
+      game.attack(player_2)
     end
   end
+
+  describe "#player1" do
+    it "shows player 1" do
+      expect(game.player1).to eq player_1
+    end
+  end
+
+  describe "#player2" do
+    it "shows player 2" do
+      expect(game.player2).to eq player_2
+    end
+  end
+
+
 end

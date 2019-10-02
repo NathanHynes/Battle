d377016ca4acb42b51a12d7e34c3a2e3340b7c49
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-10-02 17:06:59 +0100

    Showed player 2 HP on screen

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'Hit points are displayed' do
     sign_in_and_play
     expect(page).to have_content("Kirby: 100HP")
   end
+
+  scenario "see player 1's hit points" do
+    sign_in_and_play
+    expect(page).to have_content("Link: 100HP")
+  end
 end
